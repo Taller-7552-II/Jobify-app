5ac317a5ab8bc61c2034c3dcfc92fe43681434d6
gonzaloguzzardi <gonzaloguzzardi@gmail.com>
2016-12-05 23:50:41 -0300

    javasphinx

--- doc/Documentación Técnica/source/conf.py
+++ doc/Documentación Técnica/source/conf.py
@@ -21,6 +21,7 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -29,7 +30,11 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'javasphinx'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
